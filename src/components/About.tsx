1bdc0086c1403b21c2cf41babd2fddf8e7a9404f
elmaruz <lmarussig@gmail.com>
2022-01-10 16:09:43 +0000

    10.01 - About section done

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,51 @@ export default function About() {
         <h1>About</h1>
         <p className={`${s.par}`}>
           A cute little weather app built using TypeScript and React.
-          <br />
-          <br />
-          Hello mate hello mate hello mate
+          <div className={`${s.icons}`}>
+            <img src='/images/ts.png' alt='not found' className={`${s.icon}`} />
+            <img
+              src='/images/23529-2-weather-hd.png'
+              alt='not found'
+              className={`${s.icon}`}
+            />
+            <img
+              src='/images/react.png'
+              alt='not found'
+              className={`${s.icon}`}
+            />
+          </div>
+          Type in a city's name and a card will appear, displaying the current
+          temperatures and climate.
+          <div className={`${s.sep}`}>
+            Click on it to view further details, such as wind speed and
+            coordinates.
+          </div>
+          <div>
+            Weather data hosted by
+            <a href='http://api.openweathermap.org'>
+              <img
+                className={`${s.openw}`}
+                src='/images/openweather.png'
+                alt='not found'
+              />
+            </a>
+          </div>
+          <div className={`${s.sep_foot}`}>
+            <a href='https://github.com/elmaruz'>
+              <img
+                className={`${s.icon_foot}`}
+                src='/images/github.png'
+                alt='not found'
+              />
+            </a>
+            <a href='http://www.linkedin.com/in/leonardo-marussig-dev'>
+              <img
+                className={`${s.icon_foot}`}
+                src='/images/linkedin.png'
+                alt='not found'
+              />
+            </a>
+          </div>
         </p>
       </div>
     </div>
