2b1bc770aa45c55ee811d9fe958853b8803ee9cc
elmaruz <lmarussig@gmail.com>
2022-01-11 14:46:03 +0000

    11.01 App updated

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -4,7 +4,6 @@ import Cards from '../components/Cards';
 import About from '../components/About';
 import Nav from '../components/Nav';
 import Detail from '../components/Detail';
-import s from '../css_modules/App.module.css';
 
 function App(): JSX.Element {
   const APIKEY = process.env.REACT_APP_APIKEY;
