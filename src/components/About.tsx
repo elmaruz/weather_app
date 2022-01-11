6316a0680d20a65522b0d77e3eea6f896db2770a
elmaruz <lmarussig@gmail.com>
2022-01-11 16:38:50 +0000

    Responsive design complete

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <h1>About</h1>
         <p className={`${s.par}`}>
           A cute little weather app built using TypeScript and React.
-          <div className={`${s.icons}`}>
+          <p className={`${s.icons}`}>
             <img src='/images/ts.png' alt='not found' className={`${s.icon}`} />
             <img
               src='/images/23529-2-weather-hd.png'
@@ -19,40 +19,40 @@ export default function About() {
               alt='not found'
               className={`${s.icon}`}
             />
-          </div>
+          </p>
           Type in a city's name and a card will appear, displaying the current
           temperatures and climate.
-          <div className={`${s.sep}`}>
+          <p className={`${s.sep}`}>
             Click on it to view further details, such as wind speed and
             coordinates.
-          </div>
-          <div>
-            Weather data hosted by
-            <a href='http://api.openweathermap.org'>
-              <img
-                className={`${s.openw}`}
-                src='/images/openweather.png'
-                alt='not found'
-              />
-            </a>
-          </div>
-          <div className={`${s.sep_foot}`}>
-            <a href='https://github.com/elmaruz'>
-              <img
-                className={`${s.icon_foot}`}
-                src='/images/github.png'
-                alt='not found'
-              />
-            </a>
-            <a href='http://www.linkedin.com/in/leonardo-marussig-dev'>
-              <img
-                className={`${s.icon_foot}`}
-                src='/images/linkedin.png'
-                alt='not found'
-              />
-            </a>
-          </div>
+            <p>
+              Weather data hosted by
+              <a href='http://api.openweathermap.org'>
+                <img
+                  className={`${s.openw}`}
+                  src='/images/openweather.png'
+                  alt='not found'
+                />
+              </a>
+            </p>
+          </p>
         </p>
+        <div className={`${s.sep_foot}`}>
+          <a href='https://github.com/elmaruz'>
+            <img
+              className={`${s.icon_foot}`}
+              src='/images/github.png'
+              alt='not found'
+            />
+          </a>
+          <a href='http://www.linkedin.com/in/leonardo-marussig-dev'>
+            <img
+              className={`${s.icon_foot}`}
+              src='/images/linkedin.png'
+              alt='not found'
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
